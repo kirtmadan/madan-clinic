a7a50e2775143cd1455fcb7f5c20f647315b26e7
ujjwal-iqlect <ujjwal.u@iqlect.com>
2025-08-09 17:11:02 +0530

    Implement: Appointment calendar

--- lib/firebase/setup.ts
+++ lib/firebase/setup.ts
@@ -14,14 +14,15 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDKewRWTpRQ28l8ZM3cXjxEC1X0cke9COA",
-  authDomain: "madan-clinic.firebaseapp.com",
-  projectId: "madan-clinic",
-  storageBucket: "madan-clinic.firebasestorage.app",
-  messagingSenderId: "258872861879",
-  appId: "1:258872861879:web:f79f49702eec30f3c75932",
-  measurementId: "G-28K7ZMDFY2",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
