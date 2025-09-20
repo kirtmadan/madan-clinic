710d4f71bd30015e7c883310e1b4522e60fde95d
ujjwal-iqlect <ujjwal.u@iqlect.com>
2025-09-20 15:54:45 +0530

    Patient stats fixed

--- components/patients/PatientStats.tsx
+++ components/patients/PatientStats.tsx
@@ -1,28 +1,20 @@
 "use client";
 
-import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { createClient } from "@/lib/supabase/client";
 import { DescriptiveStatCard } from "@/components/reports/DescriptiveStat";
 import TimeSelector from "@/components/reports/TimeSelector";
 import dayjs from "dayjs";
+import { useTime } from "@/context/TimeContext";
 
 export default function PatientStats({ id }: { id: string }) {
-  const [timeRange, setTimeRange] = useState<string>("1d");
+  const { timeState } = useTime();
 
   const { data: completedAppointmentsCount } = useQuery({
-    queryKey: ["completedAppointmentsCount", id, timeRange],
+    queryKey: ["completedAppointmentsCount", id, timeState],
     queryFn: async () => {
-      let startDate = dayjs().startOf("month").subtract(1, "second");
-      let endDate = dayjs().endOf("month");
-
-      if (timeRange === "1d") {
-        startDate = dayjs().startOf("day").subtract(1, "second");
-        endDate = dayjs().endOf("day");
-      } else if (timeRange === "7d") {
-        startDate = dayjs().startOf("week").subtract(1, "second");
-        endDate = dayjs().endOf("week");
-      }
+      const startDate = dayjs(timeState?.from).startOf("day");
+      const endDate = dayjs(timeState?.to).endOf("day");
 
       const supabase = createClient();
 
@@ -31,7 +23,7 @@ export default function PatientStats({ id }: { id: string }) {
         .select("*", { count: "exact", head: true })
         .eq("patient_id", id)
         .eq("status", "completed")
-        .gt("date", startDate?.format("YYYY-MM-DD"))
+        .gte("date", startDate?.format("YYYY-MM-DD"))
         .lte("date", endDate?.format("YYYY-MM-DD"));
 
       return count || 0;
@@ -39,18 +31,10 @@ export default function PatientStats({ id }: { id: string }) {
   });
 
   const { data: pendingAppointmentsCount } = useQuery({
-    queryKey: ["pendingAppointmentsCount", id, timeRange],
+    queryKey: ["pendingAppointmentsCount", id, timeState],
     queryFn: async () => {
-      let startDate = dayjs().startOf("month").subtract(1, "second");
-      let endDate = dayjs().endOf("month");
-
-      if (timeRange === "1d") {
-        startDate = dayjs().startOf("day").subtract(1, "second");
-        endDate = dayjs().endOf("day");
-      } else if (timeRange === "7d") {
-        startDate = dayjs().startOf("week").subtract(1, "second");
-        endDate = dayjs().endOf("week");
-      }
+      const startDate = dayjs(timeState?.from).startOf("day");
+      const endDate = dayjs(timeState?.to).endOf("day");
 
       const supabase = createClient();
 
@@ -58,8 +42,8 @@ export default function PatientStats({ id }: { id: string }) {
         .from("appointments")
         .select("*", { count: "exact", head: true })
         .eq("patient_id", id)
-        .neq("status", "completed")
-        .gt("date", startDate?.format("YYYY-MM-DD"))
+        .or("status.eq.scheduled,status.eq.rescheduled")
+        .gte("date", startDate?.format("YYYY-MM-DD"))
         .lte("date", endDate?.format("YYYY-MM-DD"));
 
       return count || 0;
@@ -67,18 +51,10 @@ export default function PatientStats({ id }: { id: string }) {
   });
 
   const { data: totalPaymentsCount } = useQuery({
-    queryKey: ["totalPaymentsCount", id, timeRange],
+    queryKey: ["totalPaymentsCount", id, timeState],
     queryFn: async () => {
-      let startDate = dayjs().startOf("month").subtract(1, "second");
-      let endDate = dayjs().endOf("month");
-
-      if (timeRange === "1d") {
-        startDate = dayjs().startOf("day").subtract(1, "second");
-        endDate = dayjs().endOf("day");
-      } else if (timeRange === "7d") {
-        startDate = dayjs().startOf("week").subtract(1, "second");
-        endDate = dayjs().endOf("week");
-      }
+      const startDate = dayjs(timeState?.from).startOf("day");
+      const endDate = dayjs(timeState?.to).endOf("day");
 
       const supabase = createClient();
 
@@ -86,19 +62,17 @@ export default function PatientStats({ id }: { id: string }) {
         .from("payments")
         .select("total:amount")
         .eq("patient_id", id)
-        .gt("created_at", startDate?.toISOString())
-        .lte("created_at", endDate?.toISOString());
+        .gte("created_at", startDate?.format("YYYY-MM-DD HH:mm"))
+        .lte("created_at", endDate?.format("YYYY-MM-DD HH:mm"));
 
       return res?.data?.reduce((acc, item) => acc + item?.total, 0) || 0;
     },
   });
 
-  console.log({ totalPaymentsCount });
-
   return (
     <>
       <div className="col-span-3">
-        <TimeSelector timeRange={timeRange} setTimeRangeAction={setTimeRange} />
+        <TimeSelector />
       </div>
 
       <DescriptiveStatCard
@@ -116,7 +90,9 @@ export default function PatientStats({ id }: { id: string }) {
       <DescriptiveStatCard
         title="Total Payments"
         description={`Total payments made`}
-        dataToShow={("₹ " + totalPaymentsCount) as string}
+        dataToShow={
+          (totalPaymentsCount ? "₹ " + +totalPaymentsCount : 0) as string
+        }
       />
     </>
   );
